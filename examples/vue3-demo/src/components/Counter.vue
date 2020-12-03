4cdc0e76f10aa4ee53c7aeb111a3a50b17456557
xiaoluoboding <xiaoluoboding@gmail.com>
2020-12-03 14:07:53 +0800

    docs: init examples

---
+++ examples/vue3-demo/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <br>
+    <button @click="increment">+</button>
+    <br>
+    <button @click="decrement">-</button>
+    <br>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <br>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useVuex } from '@vueblock/vue-use-vuex'
+
+export default defineComponent({
+  setup () {
+    const { useGetters, useActions } = useVuex
+
+    return {
+      ...useGetters([
+        'evenOrOdd'
+      ]),
+      ...useActions([
+        'increment',
+        'decrement',
+        'incrementIfOdd',
+        'incrementAsync'
+      ])
+    }
+  }
+})
+</script>
