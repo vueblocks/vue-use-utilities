59f4589a9cdb9737a4a8538c52b6fd9eb3e3947e
xiaoluoboding <xiaoluoboding@gmail.com>
2021-01-10 21:48:40 +0800

    chore: update packages

--- scripts/packages.ts
+++ scripts/packages.ts
@@ -16,12 +16,18 @@ export const packages: PackageManifest[] = [
     display: 'VueUseUtilities',
     description: 'Collection of essential Vue Composition API',
     external: [
-      'lodash',
-      'screenfull'
+      'lodash.debounce',
+      'lodash.throttle',
+      'screenfull',
+      'keymaster',
+      '@juggle/resize-observer'
     ],
     globals: {
-      'lodash': 'lodash',
-      'screenfull': 'screenfull'
+      'lodash.debounce': 'lodash.debounce',
+      'lodash.throttle': 'lodash.throttle',
+      'screenfull': 'screenfull',
+      '@juggle/resize-observer': 'ResizeObserver',
+      'keymaster': 'keymaster'
     }
   },
   {
