52dadccf6a10b875dc4c103a89ba5627bf384e1c
xiaoluoboding <xiaoluoboding@gmail.com>
2021-01-11 11:40:35 +0800

    docs: update vue3-demo

--- examples/vue3-demo/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const isObject = (val: any): val is object => toString.call(val) === '[object Object]'
-export const isString = <T = any>(str: string | T): str is string => typeof str === 'string'
-export const partial = (fn: Function, ...partials: Array<any>) => (...args: Array<any>) => fn(...partials, ...args)
